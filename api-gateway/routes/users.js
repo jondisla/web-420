71a85d2a8049adf210f17e54afcbec6e901568cd
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-03-25 23:46:14 -0400

    user.js attribution

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+/**
+ * /*
+ * ============================================
+ * ; Title:  API-Gateway
+ * ; Author: Jonathan Disla
+ * ; Date: 25 January 2021
+ * ; Description: gateway project
+ * ;===========================================
+ *
+ * @format
+ */
+
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;

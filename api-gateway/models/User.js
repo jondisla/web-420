d660627308dd4c4654dd297435ea85012692784e
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-04-11 17:33:43 -0400

    4.2

--- api-gateway/models/User.js
+++ api-gateway/models/User.js
@@ -17,4 +17,14 @@ var userSchema = new mongoose.Schema({
   email: String,
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = (module.exports = mongoose.model("User", userSchema));
+
+// add new user to db
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = { _id: id };
+  User.findById(query, callback);
+};
